8798de9ccca45753d485f7c0762e345bd103f362
vitolinho <93949338+vitolinho@users.noreply.github.com>
2024-02-16 00:21:47 +0100

    improve ui of navbar seller and all auth pages

--- src/components/navbar/NavbarSeller.tsx
+++ src/components/navbar/NavbarSeller.tsx
@@ -1,15 +1,8 @@
-import { Link } from "@tanstack/react-router";
-
 export default function NavbarSeller() {
   return (
     <>
-      <div className="sticky top-0 w-full flex flex-row justify-between items-center px-20 py-10 backdrop-blur-xl bg-white/30">
+      <div className="sticky top-0 w-full flex flex-row justify-center items-center px-20 py-10 backdrop-blur-xl bg-white/30">
         <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRaAIQE3OWwsMoF5iYqRiMC5aZhXcR1LL8eRNucNYfAWU37YYuqdQqQezWivg&s" className="w-10"/>
-        <div className="flex flex-row gap-x-10">
-          <Link to="/seller" className="capitalize font-bold">
-            articles
-          </Link>
-        </div>
       </div>
     </>
   )
