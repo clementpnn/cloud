c87729afbec055ddcbb8c0c5133c397540fb8855
Clément <95907390+clementpnn@users.noreply.github.com>
2024-02-15 12:46:56 +0100

    delete button
    Co-Authored-By: vitolinho <93949338+vitolinho@users.noreply.github.com>

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -94,13 +94,6 @@ export default function Login() {
                     className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                   />
                 </div>
-                <Link to="/" className="text-indigo-600">
-                  Sign up as a customer
-                </Link>
-                <br />
-                <Link to="/signin" className="text-indigo-600">
-                  Sign up as a seller
-                </Link>
               </div>
 
               <div>
